097e7f2786e3820256d5e209fb1fc59bc63b5093
Greg Schechter <diabolomaster@gmail.com>
2016-11-29 07:01:38 -0800

    Remove unneeded headers

--- src/index.js
+++ src/index.js
@@ -88,18 +88,11 @@ class ebird {
             return request({
                 method: 'POST',
                 uri: url,
-                headers: {
-                    'Content-type': 'application/x-www-form-urlencoded',
-                    'Origin': 'https://secure.birds.cornell.edu',
-                    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
-                    'Referer': url,
-                },
                 form: {
                     _eventId: 'submit',
                     execution: 'e1s1',
                     lt: matches[1],
                     password: password,
-                    rememberMe: 'on',
                     username: username,
                 },
                 followAllRedirects: true,
